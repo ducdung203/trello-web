9dd0631dd61c4a88b1c47f9ebd77bdb7c0c5c690
ducdung203 <anhemvuive79@gmail.com>
2025-01-19 20:25:48 +0700

    Dark light p2

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,51 @@
 import Button from '@mui/material/Button'
 import { useColorScheme } from '@mui/material/styles'
 
+import InputLabel from '@mui/material/InputLabel'
+import MenuItem from '@mui/material/MenuItem'
+import FormControl from '@mui/material/FormControl'
+import Select from '@mui/material/Select'
+import LightModeIcon from '@mui/icons-material/LightMode'
+import SettingsBrightnessIcon from '@mui/icons-material/SettingsBrightness'
+import DarkModeIcon from '@mui/icons-material/DarkMode'
+
+function ModeSelect() {
+  const { mode, setMode } = useColorScheme()
+  const handleChange = ( event ) => {
+    const selectMode = event.target.value
+    setMode(selectMode)
+  }
+
+  return (
+    <FormControl sx={{ m: 1, minWidth: 120 }} size="small">
+      <InputLabel id="label-select-dark-light-mode">Mode</InputLabel>
+      <Select
+        labelId="label-select-dark-light-mode"
+        id="select-dark-light-mode"
+        value={mode}
+        label="Mode"
+        onChange={handleChange}
+      >
+        <MenuItem value="light">
+          <div style={ { display: 'flex', alignItems: 'center', gap: '8px' } }>
+            <LightModeIcon/> Light
+          </div>
+        </MenuItem>
+        <MenuItem value="dark">
+          <div style={ { display: 'flex', alignItems: 'center', gap: '8px' } }>
+            <DarkModeIcon/> Dark
+          </div>
+        </MenuItem>
+        <MenuItem value="system">
+          <div style={ { display: 'flex', alignItems: 'center', gap: '8px' } }>
+            <SettingsBrightnessIcon/> System
+          </div>
+        </MenuItem>
+      </Select>
+    </FormControl>
+  )
+}
+
 function ModeToggle() {
   const { mode, setMode } = useColorScheme()
   return (
@@ -17,10 +62,12 @@ function ModeToggle() {
 function App() {
   return (
     <>
+      <ModeSelect/>
+      <hr/>
       <ModeToggle/>
       <hr/>
       <div>ducdung203</div>
-      <Button variant="text" >Text</Button>
+      <Button variant="text" > Text</Button>
       <Button Button variant="contained" >Contained</Button>
       <Button variant="outlined" >Outlined</Button>
     </>
