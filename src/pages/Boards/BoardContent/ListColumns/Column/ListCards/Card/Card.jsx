3f945e4ad2c8f4f62b59fdf0671ce82b247fd225
ducdung203 <anhemvuive79@gmail.com>
2025-01-27 23:20:48 +0700

    drap drop card p1

--- src/pages/Boards/BoardContent/ListColumns/Column/ListCards/Card/Card.jsx
+++ src/pages/Boards/BoardContent/ListColumns/Column/ListCards/Card/Card.jsx
@@ -1,14 +1,30 @@
+import { useSortable } from '@dnd-kit/sortable'
+import { CSS } from '@dnd-kit/utilities'
 import AttachmentIcon from '@mui/icons-material/Attachment'
 import CommentIcon from '@mui/icons-material/Comment'
 import GroupIcon from '@mui/icons-material/Group'
-import Button from '@mui/material/Button'
 import { Card as MuiCard } from '@mui/material'
+import Button from '@mui/material/Button'
 import CardActions from '@mui/material/CardActions'
 import CardContent from '@mui/material/CardContent'
 import CardMedia from '@mui/material/CardMedia'
 import Typography from '@mui/material/Typography'
 
 function Card({ card }) {
+  const {
+    attributes,
+    listeners,
+    setNodeRef,
+    transform,
+    transition,
+    isDragging
+  }= useSortable({ id: card._id, data: { ...card } })
+  const dndKitCardStyles = {
+    touchAction: 'none',
+    transform: CSS.Translate.toString(transform),
+    transition,
+    opacity:isDragging?0.5:undefined
+  }
   // if () {
   //   return (
   //     <MuiCard sx={{
@@ -26,7 +42,7 @@ function Card({ card }) {
     return !!card?.memberIds?.length||!!card?.comments?.length||!!card?.attachments?.length
   }
   return (
-    <MuiCard sx={{
+    <MuiCard ref={setNodeRef} style={dndKitCardStyles} {...attributes} {...listeners} sx={{
       cursor:'pointer',
       boxShadow:'0 1px 1px rgba(0,0,0,0.2)',
       overflow:'unset'
