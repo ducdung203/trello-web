355f0a66599127f63e4c552b88d58e65ff53070a
ducdung203 <anhemvuive79@gmail.com>
2025-03-21 22:44:01 +0700

    mongdb aggregate

--- src/pages/Boards/BoardBar/BoardBar.jsx
+++ src/pages/Boards/BoardBar/BoardBar.jsx
@@ -41,11 +41,13 @@ function BoardBar({ board }) {
       '&::-webkit-scrollbar-track':{ m:2 }
     }}>
       <Box sx={{ display: 'flex', alignItems:'center', gap: 2 }}>
-        <Chip sx={MENU_STYLE}
-          icon={<DashboardIcon />}
-          label={board?.title }
-          clickable
-        />
+        <Tooltip title={board?.description}>
+          <Chip sx={MENU_STYLE}
+            icon={<DashboardIcon />}
+            label={board?.title }
+            clickable
+          />
+        </Tooltip>
         <Chip sx={MENU_STYLE}
           icon={<VpnLockIcon />}
           label={ capitalizeFirstLetter(board?.type)}
